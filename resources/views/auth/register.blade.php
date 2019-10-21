e86ec10ec6adf8a025a595c55c51fce749b39282
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-10-22 00:18:13 +0300

    first project Act

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,75 @@
+@extends('auth.master')
+
+@section('pageTitle','Registration')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                <div class="login-brand">
+                    <img src="../assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                </div>
+
+                <div class="card card-primary">
+                    <div class="card-header"><h4>Register</h4></div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="row">
+                                <div class="form-group col-6">
+                                    <label for="name">Name</label>
+                                    <input id="name" type="text" class="form-control" name="name" required autofocus autocomplete="false" value="{{ old('name') }}">
+                                    @error('name')
+                                        <div class="invalid-feedback">
+                                            <strong>{{ $message }}</strong>
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group col-6">
+                                        <label for="email">Email</label>
+                                        <input id="email" type="email" class="form-control" name="email" autocomplete="false" value="{{ old('email') }}">
+                                        @error('email')
+                                        <div class="invalid-feedback">
+                                            <strong>{{ $message }}</strong>
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group col-6">
+                                    <label for="password" class="d-block">Password</label>
+                                    <input id="password" type="password" class="form-control pwstrength" autocomplete="new-password"
+                                           data-indicator="pwindicator" name="password" required>
+                                    <div id="pwindicator" class="pwindicator">
+                                        <div class="bar"></div>
+                                        <div class="label"></div>
+                                    </div>
+                                </div>
+                                <div class="form-group col-6">
+                                    <label for="password2" class="d-block">Password Confirmation</label>
+                                    <input id="password2" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">
+                                    Register
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="simple-footer">
+                    Copyright &copy; {{ config('app.name') }} 2018
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('footerJS')
+<script src="{{ asset('assets/plugins/jquery.pwstrength/pwstrength-bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/page/auth-register.js') }}"></script>
+@endsection
